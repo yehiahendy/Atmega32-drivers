43b135eed6b7830fb742f9852758a423f3db7f2a
yehiahendy <yayahendy11@gmail.com>
2020-10-02 07:56:39 +0200

    Edit ADC.c

--- MCAL/ADC/ADC.c
+++ MCAL/ADC/ADC.c
@@ -7,11 +7,7 @@
 #include "ADC.h"
 void ADC_init(ADC_ch Channel)
 {
-	//Enable ADC
-	SET_Bit(ADCSRA,ADEN);
-	//choose the channel 
-	ADMUX = 0x00;
-	ADMUX |= Channel;
+
 	//choose the Voltage reference 
 	ADMUX &= ADC_VoltageReference_clr_msk;
 	ADMUX |= Voltage_Reference_Selector;
@@ -28,5 +24,40 @@ void ADC_init(ADC_ch Channel)
 	SFIOR |= ADC_Trigger_Selector;
 	SET_Bit(ADCSRA,ADSC);
 	#endif
+		//Enable ADC
+		SET_Bit(ADCSRA,ADEN);
+		//choose the channel
+		ADMUX &= 0b11100000;
+		ADMUX |= Channel;
+}
+uint16 ADC_ReadChannel(ADC_ch Channel)
+{
+	#if (ADC_Mode_Selector == ADC_SingleConversion_msk)
+	ADMUX &= 0b11100000;
+	ADMUX |= Channel;
+	SET_Bit(ADCSRA,ADSC);
+#endif
+	while(GET_Bit(ADCSRA,ADIF) == 0)
+	{
+		;
+	}
+	return ADC;
+}
+void ADC_enInterrupt(void)
+{
+	SET_Bit(ADCSRA,ADIE);
 }
 
+void ADC_diInterrupt(void)
+{
+	CLEAR_Bit(ADCSRA,ADIE);
+}
+
+uint32 ADC_getVoltage_m(ADC_ch Channel)
+{
+	uint32 StepsNum;
+	StepsNum=ADC_ReadChannel(Channel);
+
+	return StepsNum*(ADC_VOLTAGE_REF_mv/ADC_TOTAL_STEPS);
+
+}
